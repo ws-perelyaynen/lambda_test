fde4264ff9d9d0bb4ffe9bb7d9c38606ea2cea13
Alex_P <alex.perelyaynen@brainbankers.com>
2018-03-07 16:57:56 +0300

    test

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-var a = 3;
+// const express = require('express');
+// const app = express();
+
+// app.get('/', (req, res) => res.send('Hello World!'));
+
+// app.listen(3000, () => console.log('Example app listening on port 3000!'));
+const fs = require('fs');
+exports.handler = (event, context, callback) => {
+	// TODO implement
+	console.log(`**event: ${event}`);
+	console.log(`**context: ${context}`);
+	var file = fs.readFileSync('./index.html');
+	callback(null, file);
+};
