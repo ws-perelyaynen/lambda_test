8c5dd82b18c65d27ab7c7242ee22862bb0582037
Alex_P <alex.perelyaynen@brainbankers.com>
2018-07-26 13:19:52 +0300

    packages

--- index.js
+++ index.js
@@ -4,11 +4,10 @@
 // app.get('/', (req, res) => res.send('Hello World!'));
 
 // app.listen(3000, () => console.log('Example app listening on port 3000!'));
-const fs = require('fs');
 exports.handler = (event, context, callback) => {
 	// TODO implement
 	console.log(`**event: ${event}`);
 	console.log(`**context: ${context}`);
-	var file = fs.readFileSync('./index.html');
-	callback(null, file);
+	callback(null, 'Im here');
+	// callback(null, file);
 };
